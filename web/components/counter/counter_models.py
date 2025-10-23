fcf2179681e2b13f089410d2cfd1d82f72c958ea
Marcos Macedo <marcos@marcos-mac.local>
2025-10-22 22:17:43 -0400

    fix: Correct various minor bugs and inconsistencies

--- web/components/counter/counter_models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class Counter(Base):
-    __tablename__ = 'counter'
-    id = Column(Integer, primary_key=True)
-    count = Column(Integer, default=0)
