182a94267dd065cdfbe7d8372e26ba11ed2f9a84
Marcos Macedo <marcosmacedo30@hotmail.com>
2025-10-28 09:46:07 -0400

    Temporarily disabled some tools

--- framework/src/disco/interactive_tools/parser.rs
+++ framework/src/disco/interactive_tools/parser.rs
@@ -3,34 +3,34 @@ use crate::disco::interactive_tools::models::InteractiveTool;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 
-const API_INTEGRATION_HELPER: &str =
-    include_str!("tools_yaml/api_integration_helper.yaml");
-const COMPONENT_GENERATOR: &str =
-    include_str!("tools_yaml/component_generator.yaml");
-const CONFIG_EXPLAINER: &str =
-    include_str!("tools_yaml/config_explainer.yaml");
+// const API_INTEGRATION_HELPER: &str =
+//     include_str!("tools_yaml/api_integration_helper.yaml");
+// const COMPONENT_GENERATOR: &str =
+//     include_str!("tools_yaml/component_generator.yaml");
+// const CONFIG_EXPLAINER: &str =
+//     include_str!("tools_yaml/config_explainer.yaml");
 const DATABASE_MIGRATION_HELPER: &str =
     include_str!("tools_yaml/database_migration_helper.yaml");
-const DYNAMIC_ROUTING_EXPLAINER: &str =
-    include_str!("tools_yaml/dynamic_routing_explainer.yaml");
-const NEW_PAGE_CREATOR: &str =
-    include_str!("tools_yaml/new_page_creator.yaml");
-const ONBOARDING_GUIDE: &str =
-    include_str!("tools_yaml/onboarding_guide.yaml");
-const STATIC_FILE_GUIDE: &str =
-    include_str!("tools_yaml/static_file_guide.yaml");
+// const DYNAMIC_ROUTING_EXPLAINER: &str =
+//     include_str!("tools_yaml/dynamic_routing_explainer.yaml");
+// const NEW_PAGE_CREATOR: &str =
+//     include_str!("tools_yaml/new_page_creator.yaml");
+// const ONBOARDING_GUIDE: &str =
+//     include_str!("tools_yaml/onboarding_guide.yaml");
+// const STATIC_FILE_GUIDE: &str =
+//     include_str!("tools_yaml/static_file_guide.yaml");
 
 static TOOLS: Lazy<HashMap<String, InteractiveTool>> = Lazy::new(|| {
     let mut tools = HashMap::new();
     let tool_files = vec![
-        API_INTEGRATION_HELPER,
-        COMPONENT_GENERATOR,
-        CONFIG_EXPLAINER,
+        // API_INTEGRATION_HELPER,
+        // COMPONENT_GENERATOR,
+        // CONFIG_EXPLAINER,
         DATABASE_MIGRATION_HELPER,
-        DYNAMIC_ROUTING_EXPLAINER,
-        NEW_PAGE_CREATOR,
-        ONBOARDING_GUIDE,
-        STATIC_FILE_GUIDE,
+        // DYNAMIC_ROUTING_EXPLAINER,
+        // NEW_PAGE_CREATOR,
+        // ONBOARDING_GUIDE,
+        // STATIC_FILE_GUIDE,
     ];
 
     for content in tool_files {
